039086362a8061b5f906c019864dd4adbf985314
MiniMarker <cfmarker@gmail.com>
2020-06-07 18:05:32 +0200

    fixed bug where perferredLanguage was not set.

--- src/config/theme/theme.ts
+++ src/config/theme/theme.ts
@@ -4,7 +4,7 @@ export const theme = {
     mediumGreyBackground: 'rgb(54,54,54)',
     lightGreyBackground: 'rgb(235,235,235)',
     lightBackground: 'rgb(255,255,255)',
-    textLight: 'rgb(230,230,230)',
+    textLight: 'rgb(255,255,255)',
     textDark: 'rgb(26,26,26)',
     primaryColor: 'rgb(64,105,151)',
     primaryColorHover: 'rgb(61,99,143)',
